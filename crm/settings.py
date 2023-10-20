3fd97e37e24680c6b944af11f5249248abb320d0
isaacsabbath <isaacsabbath002@gmail.com>
2023-10-20 17:26:57 +0300

    initial commit

--- crm/settings.py
+++ crm/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sumaiyabinte580@gmail.com'
-EMAIL_HOST_PASSWORD = 'sumaiya1122334455binte'
+EMAIL_HOST_USER = 'isaacevery002@gmail.com'
+EMAIL_HOST_PASSWORD = 'warukenya002'
 
 
 
@@ -154,3 +154,4 @@ EMAIL_HOST_PASSWORD = 'sumaiya1122334455binte'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATABASE_NAME = '/home/sabbath/Desktop/CRM--Django/db.sqlite3'
